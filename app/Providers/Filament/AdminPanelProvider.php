4773479145ad1da62c4a59ea0e686d9d271853c6
Raphnesia <raphnesia@gmail.com>
2025-08-14 12:40:25 +0700

    fix: revert to default login to resolve 500 error - Temporarily disable custom login and 2FA features - Comment out Google 2FA methods in User model - Remove 2FA page from navigation - This will restore basic login functionality

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(\App\Filament\Pages\CustomLogin::class)
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -36,7 +36,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
-                \App\Filament\Pages\TwoFactorSetup::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
